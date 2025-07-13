6bef9bbb4379142d6715efc447ce5672e39a8907
ganmingzhong <76235514+ganmingzhong@users.noreply.github.com>
2025-07-13 19:41:51 +0800

    Update supertrend_my.py

--- supertrend_my.py
+++ supertrend_my.py
@@ -42,7 +42,7 @@ def send_slack_message(webhook_url, message):
 def get_data(name):
     # Calculate the date range for the months
     end_date = datetime.today()
-    start_date = end_date - timedelta(days=1000)
+    start_date = end_date - timedelta(days=2000)
     df = yf.download(name, start=start_date, end=end_date, auto_adjust=True)[['High','Close','Low']]
 
     # Flatten MultiIndex columns if they exist
